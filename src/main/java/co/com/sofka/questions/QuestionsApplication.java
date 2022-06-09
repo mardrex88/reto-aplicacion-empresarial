34ac0effdc62689b15b4b211de1847a6ef60fc98
Camila Morales <camila.moralesg21@gmail.com>
2022-06-09 17:56:40 -0500

    Documentacion API Se realizo un cambio de dependencias para usar la biblioteca OpenApi y la interfaz de usuario swagger. Se usaron las anotaciones en el QuestionRouter.java para documentar la api.

--- src/main/java/co/com/sofka/questions/QuestionsApplication.java
+++ src/main/java/co/com/sofka/questions/QuestionsApplication.java
@@ -3,8 +3,13 @@ package co.com.sofka.questions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+
 @SpringBootApplication
+@EnableReactiveMongoRepositories
 public class QuestionsApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(QuestionsApplication.class, args);
--- src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
+++ src/main/java/co/com/sofka/questions/routers/QuestionRouter.java
@@ -3,6 +3,14 @@ package co.com.sofka.questions.routers;
 import co.com.sofka.questions.model.AnswerDTO;
 import co.com.sofka.questions.model.QuestionDTO;
 import co.com.sofka.questions.usecases.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -19,7 +27,14 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class QuestionRouter {
 
+    //Get all question
     @Bean
+    @RouterOperation(beanClass = ListUseCase.class, beanMethod = "get",
+            operation = @Operation(operationId = "listAllQuestions", summary = "List all questions", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> getAll(ListUseCase listUseCase) {
         return route(GET("/getAll"),
                 request -> ServerResponse.ok()
@@ -28,7 +43,15 @@ public class QuestionRouter {
         );
     }
 
+    //Get all questions user
     @Bean
+    @RouterOperation(beanClass = OwnerListUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "getAllQuestionsUser", summary = "Get all questions from a user", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "400", description = "Invalid User ID supplied", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                             @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                             @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                             @ApiResponse(responseCode = "404", description = "User not found", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> getOwnerAll(OwnerListUseCase ownerListUseCase) {
         return route(
                 GET("/getOwnerAll/{userId}"),
@@ -37,13 +60,20 @@ public class QuestionRouter {
                         .body(BodyInserters.fromPublisher(
                                 ownerListUseCase.apply(request.pathVariable("userId")),
                                 QuestionDTO.class
-                         ))
+                        ))
         );
     }
 
+    //Post question
     @Bean
+    @RouterOperation(beanClass = CreateUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "createQuestion", summary = "Create Question", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "201", description = "Created"),
+                             @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                             @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                             @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> create(CreateUseCase createUseCase) {
-        Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO ->  createUseCase.apply(questionDTO)
+        Function<QuestionDTO, Mono<ServerResponse>> executor = questionDTO -> createUseCase.apply(questionDTO)
                 .flatMap(result -> ServerResponse.ok()
                         .contentType(MediaType.TEXT_PLAIN)
                         .bodyValue(result));
@@ -54,20 +84,34 @@ public class QuestionRouter {
         );
     }
 
+    //Get question by id
     @Bean
+    @RouterOperation(beanClass = GetUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "getQuestionById", summary = "Get question by id", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> get(GetUseCase getUseCase) {
         return route(
                 GET("/get/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
                         .body(BodyInserters.fromPublisher(getUseCase.apply(
-                                request.pathVariable("id")),
+                                        request.pathVariable("id")),
                                 QuestionDTO.class
                         ))
         );
     }
 
+    //Post answer
     @Bean
+    @RouterOperation(beanClass = AddAnswerUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "addAnAnswer", summary = "Add an answer to a question", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "201", description = "Created"),
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "404", description = "Not found", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> addAnswer(AddAnswerUseCase addAnswerUseCase) {
         return route(POST("/add").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(AnswerDTO.class)
@@ -79,7 +123,14 @@ public class QuestionRouter {
         );
     }
 
+    //Delete Question
     @Bean
+    @RouterOperation(beanClass = DeleteUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "deleteQuestion", summary = "Delete question by id", tags = {"Questions"},
+                    responses = {@ApiResponse(responseCode = "200", description = "OK"),
+                            @ApiResponse(responseCode = "204", description = "No content", content = { @Content(examples = { @ExampleObject(value = "")})}), 
+                            @ApiResponse(responseCode = "401", description = "Unauthorized", content = { @Content(examples = { @ExampleObject(value = "")})}),
+                            @ApiResponse(responseCode = "403", description = "Forbidden", content = { @Content(examples = { @ExampleObject(value = "")})})}))
     public RouterFunction<ServerResponse> delete(DeleteUseCase deleteUseCase) {
         return route(
                 DELETE("/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
