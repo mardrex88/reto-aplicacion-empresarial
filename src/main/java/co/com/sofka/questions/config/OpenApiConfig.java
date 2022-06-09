9e11b6a864b18c7ebf6c34f8bf11ec0a5c88a8c3
Camila Morales <camila.moralesg21@gmail.com>
2022-06-09 18:01:17 -0500

    Configuracion OpenApi Se realizo una definicion de un bean de configuracion Spring de OpenApi, donde detallamos el titulo, la descripcion y la version de la api.

---
+++ src/main/java/co/com/sofka/questions/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package co.com.sofka.questions.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Reto Aplicación Empresarial")
+                        .description("API")
+                        .version("v1.0")
+                );
+
+    }
+}
